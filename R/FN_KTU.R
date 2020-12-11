a6b24352e284fc65809a0760fed17c84cb0ab40e
poyuliu <cryptolaemus.montrouzieri@gmail.com>
2020-12-11 13:48:21 +0800

    readme file

---
+++ R/FN_KTU.R
@@ -0,0 +1,330 @@
+#' Generate a kmer table from representative sequences
+#'
+#' Convert DNA sequences to tetranucleotide frequency table
+#' @param repseq The fasta file path
+#' @param file logical, default is TRUE, loading DNA sequences from file. if 'file=FALSE', the 'repseq' is loaded from object.
+#' @return A 256 tetranucleotide combinations frequency table
+#' @export
+tetra.freq <- function(repseq,file=TRUE){
+  rev_comp <- function(x){
+    x.r <- paste0(rev(strsplit(x,"")[[1]]),collapse = "")
+    x.r <- gsub("A","1",x.r);x.r <- gsub("T","2",x.r);x.r <- gsub("C","3",x.r);x.r <- gsub("G","4",x.r)
+    x.r <- gsub("1","T",x.r);x.r <- gsub("2","A",x.r);x.r <- gsub("3","G",x.r);x.r <- gsub("4","C",x.r)
+    return(x.r)
+  }
+  readseq <- function(seq){
+    x <- seq
+    x <- paste0(x,collapse = "")
+    x.r <- rev_comp(x)
+    return(list(x,x.r))
+  }
+  if(isTRUE(file)){
+    scaffolds = Biostrings::readDNAStringSet(filepath = repseq, use.names = T)
+  } else scaffolds = Biostrings::DNAStringSet(repseq)
+  asv.id <- scaffolds@ranges@NAMES
+  species <- lapply(scaffolds, function(x) readseq(as.character(x)))
+
+  DNA <- c("A","T","C","G")
+  tetra.mer <- expand.grid(DNA,DNA,DNA,DNA)
+  tetra.mer <- do.call(paste0,tetra.mer)
+
+  tetra.table <- matrix(nrow=256,ncol=length(species))
+  rownames(tetra.table) <- tetra.mer
+
+  #full length ASV
+  for(i in 1:256){
+    for(j in 1:length(species)){
+      single.forward <- ifelse(length(grep(tetra.mer[i],species[[j]][[1]]))>0,grep(tetra.mer[i],species[[j]][[1]]),0)
+      single.reverse <- ifelse(length(grep(tetra.mer[i],species[[j]][[2]]))>0,grep(tetra.mer[i],species[[j]][[2]]),0)
+      tetra.table[i,j] <- (single.forward+single.reverse)
+    }
+  }
+  colnames(tetra.table) <- asv.id
+  tetra.table <- prop.table(tetra.table,2)
+  return(tetra.table)
+}
+
+
+#' KTU clustering
+#'
+#' K-mer taxonomic unit clustering
+#' @param repseq The fasta file path
+#' @param feature.table (optional) 'data.frame' formated ASV/OTU table
+#' @param write.fasta (optional) write out a representative KTU sequence fasta file
+#' @param step Split searching range for optimal K. Two step searching process: large scale searching in the first round and smaller scale searching in the second round. Default is 'step=c(3,10)'.
+#' @param search.min The minimum K for searching, default 'search.min=10'
+#' @param search.max The maximum K for searching, default is 'NULL' = max
+#' @param cores Numbers of CPUs
+#' @return KTU.table aggregated KTU table
+#' @return ReqSeq Representative KTU sequences
+#' @return kmer.table Tetranucleotide frequency table
+#' @return clusters K-clusters of input features
+#' @export
+klustering <- function(repseq,feature.table=NULL,write.fasta=TRUE,step=c(3,10),search.min=10,search.max=NULL,cores=1){
+  require(foreach,quietly = T)
+  rev_comp <- function(x){
+    x.r <- paste0(rev(strsplit(x,"")[[1]]),collapse = "")
+    x.r <- gsub("A","1",x.r);x.r <- gsub("T","2",x.r);x.r <- gsub("C","3",x.r);x.r <- gsub("G","4",x.r)
+    x.r <- gsub("1","T",x.r);x.r <- gsub("2","A",x.r);x.r <- gsub("3","G",x.r);x.r <- gsub("4","C",x.r)
+    return(x.r)
+  }
+  readseq <- function(seq){
+    x <- seq
+    x <- paste0(x,collapse = "")
+    x.r <- rev_comp(x)
+    return(list(x,x.r))
+  }
+  aggregate2df <- function(data,groups,FUN){
+    agg.data <- aggregate(data,list(groups),FUN)
+    rownames(agg.data) <- agg.data[,1]
+    agg.data <- as.data.frame(t(agg.data[,-1]))
+    return(agg.data)
+  }
+
+  scaffolds = Biostrings::readDNAStringSet(filepath = repseq, use.names = T)
+  asv.id <- scaffolds@ranges@NAMES
+  species <- lapply(scaffolds, function(x) readseq(as.character(x)))
+
+  DNA <- c("A","T","C","G")
+  tetra.mer <- expand.grid(DNA,DNA,DNA,DNA)
+  tetra.mer <- do.call(paste0,tetra.mer)
+
+  tetra.table <- matrix(nrow=256,ncol=length(species))
+  rownames(tetra.table) <- tetra.mer
+
+  #full length ASV
+  for(i in 1:256){
+    for(j in 1:length(species)){
+      single.forward <- ifelse(length(grep(tetra.mer[i],species[[j]][[1]]))>0,grep(tetra.mer[i],species[[j]][[1]]),0)
+      single.reverse <- ifelse(length(grep(tetra.mer[i],species[[j]][[2]]))>0,grep(tetra.mer[i],species[[j]][[2]]),0)
+      tetra.table[i,j] <- (single.forward+single.reverse)
+    }
+  }
+  colnames(tetra.table) <- asv.id
+  tetra.table <- prop.table(tetra.table,2)
+  cos <- as.dist(1-coop::cosine(tetra.table))
+
+  cl <- parallel::makeCluster(cores) #not to overload your computer
+  doParallel::registerDoParallel(cl)
+
+  SS <- length(species)
+  if(is.null(search.max)) search.max <- SS-1 else search.max <- search.max
+  steps <- ceiling(seq(search.min,search.max,length.out = step[1]))
+  repeat{
+    steps.update <- steps
+    asw <- foreach::foreach(k=steps.update, .combine=c) %dopar% {
+      temp.asw = cluster::pam(cos,k,do.swap = F,pamonce = 5)$silinfo$avg.width #calling a function
+      temp.asw #Equivalent to finalMatrix = cbind(finalMatrix, tempMatrix)
+    }
+    k.best <- steps.update[order(asw,decreasing = T)][1:2]
+    print(k.best)
+    steps <- ceiling(seq(k.best[2],k.best[1],length.out = step[1]))
+    if(all(steps==steps.update)) break
+  }
+  step <- ceiling(seq(k.best[2],k.best[1],length.out = step[2]))
+  rm(k.best)
+  repeat{
+    steps.update <- steps
+    asw <- foreach::foreach(k=steps.update, .combine=c) %dopar% {
+      temp.asw = cluster::pam(cos,k,do.swap = F,pamonce = 5)$silinfo$avg.width #calling a function
+      temp.asw #Equivalent to finalMatrix = cbind(finalMatrix, tempMatrix)
+    }
+    k.best <- steps.update[order(asw,decreasing = T)][1:2]
+    print(k.best)
+    steps <- unique(ceiling(seq(k.best[2],k.best[1],length.out = step[2])))
+    if(all(steps==steps.update)) break
+  }
+  k.best <- steps.update[which.max(asw)]
+  print(paste("Best KTU #:",k.best))
+  parallel::stopCluster(cl) #stop cluster
+
+
+  kms <- cluster::pam(cos,k.best,do.swap = F,pamonce = 5)
+  #centoid rep seq
+  centroid <- kms$id.med
+  crepseq <- sapply(species[centroid], '[[',1)
+  #names(crepseq) <- paste0("KTU",sprintf("%0004i",1:length(centroid)))
+  for(i in 1:length(centroid)) names(crepseq)[i] <- digest::digest(crepseq[i],algo = "md5",serialize = F)
+
+  #kmer table
+  kmer.table <- data.frame(tetra.table[,centroid])
+  colnames(kmer.table) <- names(crepseq)
+
+  if(isTRUE(write.fasta)){
+    repseq <- matrix(rbind(paste0(">",names(crepseq)),crepseq),ncol=1)
+    write(repseq,file = "ktu-sequence.fasta")
+  }
+
+  if(!is.null(feature.table)){
+    feature.table <- feature.table[match(asv.id,feature.table[,1],nomatch = 0),]
+    otu <- feature.table[,-1]
+    ktu <- as.data.frame(t(aggregate2df(otu,kms$clustering,sum)))
+    rownames(ktu) <- names(crepseq)
+    colnames(ktu) <- colnames(otu)
+    return(list(KTU.table=ktu, ReqSeq=crepseq, kmer.table=kmer.table, clusters=kms$clustering))
+  } else return(list(ReqSeq=crepseq, kmer.table=kmer.table, clusters=kms$clustering))
+}
+
+
+#' Prepare KTU taxonomy assignment database by trimming primers
+#'
+#' Trim primer sequences of full length 16S/18S/target gene database by PCR primers
+#' @param fasta input database fasta file
+#' @param forseq forward primer sequence (5' -> 3')
+#' @param revseq reverse primer sequence (5' -> 3')
+#' @param output.name (optional) the name for output file
+#' @param progress show the processing progress
+#' @export
+trim.primer <- function(fasta,forseq,revseq,output.name=NULL,progress=TRUE){
+  #read fasta file
+  fastasets <- Biostrings::readDNAStringSet(fasta)
+  seqNames <- fastasets@ranges@NAMES
+  #format primer seq & reverse complement
+  Lpattern=Biostrings::DNAString(forseq)
+  Rpattern=Biostrings::reverseComplement(Biostrings::DNAString(revseq))
+  #search primer position
+  Forward <- Biostrings::vmatchPattern(pattern = Lpattern,subject = fastasets,fixed = F)
+  Reverse <- Biostrings::vmatchPattern(pattern = Rpattern,subject = fastasets,fixed = F)
+
+  trimmed <- c()
+  for(i in 1:length(fastasets)){
+    if(length(Forward[[i]]) > 0 & length(Reverse[[i]]) >0 ){
+      Fstart <- ifelse(length(Forward[[i]])>1,Forward[[i]][1]@start,Forward[[i]]@start)
+      Fwidth <- ifelse(length(Forward[[i]])>1,Forward[[i]][1]@width,Forward[[i]]@width)
+      Rend   <- ifelse(length(Reverse[[i]])>1,Reverse[[i]][length(Reverse[[i]])]@start,Reverse[[i]]@start)
+      if(Rend-(Fstart+Fwidth)>0){
+      trimmed[i] <- as.character(fastasets[[i]][(Fstart+Fwidth):(Rend-1)])
+      } else trimmed[i] <- NA
+    } else trimmed[i] <- NA
+
+    if(progress==TRUE){
+      print(paste0(i,"/",length(fastasets)," sequences from DB are processed"))
+      flush.console()
+      Sys.sleep(0.01)
+    }
+  }
+
+  names(trimmed) <- seqNames
+  trimmed <- trimmed[!is.na(trimmed)]
+  output <- matrix(rbind(paste0(">",names(trimmed)),trimmed),ncol=1)
+  if(is.null(output.name)){
+    write(output,file = "trimmed-sequence.fasta")
+  } else if(!is.null(output.name)) write(output,file = paste0(output.name,"_trimmed-sequence.fasta"))
+}
+
+#' Build KTU taxonomy assignment database by tetranucleotide frequency table
+#'
+#' make KTU db
+#' @param input.fasta input database fasta file (trimmed)
+#' @param input.taxa input database taxonomy file
+#' @param output.file (optional) the directory for output RDS format file
+#' @export
+makektudb <- function(input.fasta,input.taxa,output.file=NULL){
+  rev_comp <- function(x){
+    x.r <- paste0(rev(strsplit(x,"")[[1]]),collapse = "")
+    x.r <- gsub("A","1",x.r);x.r <- gsub("T","2",x.r);x.r <- gsub("C","3",x.r);x.r <- gsub("G","4",x.r)
+    x.r <- gsub("1","T",x.r);x.r <- gsub("2","A",x.r);x.r <- gsub("3","G",x.r);x.r <- gsub("4","C",x.r)
+    return(x.r)
+  }
+  readseq <- function(seq){
+    x <- seq
+    x <- paste0(x,collapse = "")
+    x.r <- rev_comp(x)
+    return(list(x,x.r))
+  }
+  scaffolds = Biostrings::readDNAStringSet(filepath = input.fasta, use.names = T)
+
+  DNA <- c("A","T","C","G")
+  tetra.mer <- expand.grid(DNA,DNA,DNA,DNA)
+  tetra.mer <- do.call(paste0,tetra.mer)
+
+  species <- lapply(scaffolds, readseq)
+
+  tetra.table <- matrix(nrow=256,ncol=length(species))
+  rownames(tetra.table) <- tetra.mer
+
+  for(i in 1:256){
+    for(j in 1:length(species)){
+      single.forward <- ifelse(length(grep(tetra.mer[i],species[[j]][[1]]))>0,grep(tetra.mer[i],species[[j]][[1]]),0)
+      single.reverse <- ifelse(length(grep(tetra.mer[i],species[[j]][[2]]))>0,grep(tetra.mer[i],species[[j]][[2]]),0)
+      tetra.table[i,j] <- (single.forward+single.reverse)
+    }
+  }
+
+  colnames(tetra.table) <- scaffolds@ranges@NAMES
+  tetra.table <- prop.table(tetra.table,2)
+
+  taxa <- read.delim(input.taxa,header = F)
+  taxa <- taxa[match(scaffolds@ranges@NAMES,taxa[,1]),]
+
+  if(!is.null(output.file)){
+    saveRDS(tetra.table,file = paste0(output.file,"_DB.RDS"))
+    saveRDS(taxa,file = paste0(output.file,"_TX.RDS"))
+  }
+  return(list(tetra.table,taxa))
+}
+
+#' KTU annotation
+#'
+#' KTU-based alignment-free taxonomy assignment
+#' @param dbRDS Kmer formated database RDS file
+#' @param taxaRDS taxonomy ID RDS file
+#' @param kmer.table tetranucleotide frequency table for annotation
+#' @param cos.cutff cosine similarity cutoff, default=0.95
+#' @param consensus taxonomy assignment by consensus [0-1]
+#' @param candidate how many candidates (≥ cos.cutff) for taxonomy assignment
+#' @param cores Numbers of CPUs
+#' @export
+kaxonomy <- function(dbRDS,taxaRDS,kmer.table,cos.cutff=0.95,consensus=0.8,candidate=10,cores=1){
+  require(foreach,quietly = T)
+  cl <- parallel::makeCluster(cores) #not to overload your computer
+  doParallel::registerDoParallel(cl)
+
+  db.tetra <- readRDS(dbRDS)
+  db.taxa <- readRDS(taxaRDS)
+  {
+    taxa <- as.character(db.taxa[,2])
+    sep <- ifelse(grepl("; ",taxa[1],fixed = T),"; ",";")
+    taxa <- strsplit(taxa,sep)
+    taxa <- data.frame(do.call(rbind,taxa),row.names = db.taxa[,1])
+  }
+  cos.table <- data.frame(matrix(data = NA,nrow = nrow(db.taxa),ncol=ncol(kmer.table)))
+  cos.table <- foreach::foreach(cln=1:ncol(kmer.table), .combine=cbind) %dopar% {
+    temp.cos.table = apply(db.tetra,2,function(x) coop::cosine(kmer.table[,cln],x)) #calling a function
+    temp.cos.table #Equivalent to finalMatrix = cbind(finalMatrix, tempMatrix)
+  }
+  parallel::stopCluster(cl) #stop cluster
+
+  machine.core <- function(x){
+    xxx <- matrix(ncol = 8)
+    xx <- cbind(taxa[which(x>=cos.cutff),],cos.score=x[which(x>=cos.cutff)])
+    if(nrow(xx)>0){
+      #xx <- xx[xx$cos.score>=quantile(xx$cos.score,top.pct,na.rm=TRUE),]
+      ranks <- rank(1/(xx$cos.score))
+      if(min(ranks)>candidate){
+        xx <- xx[which(ranks==min(ranks)),]
+      } else xx <- xx[which(ranks<=candidate),]
+      xx[,-8] <- apply(xx[,-8], 2, as.character)
+      xy <- apply(xx,2,function(x) names(table(x))[which(prop.table(table(x))>consensus)])
+      xxx[,1:7] <- c(xy[[1]][1],xy[[2]][1],xy[[3]][1],xy[[4]][1],xy[[5]][1],xy[[6]][1],xy[[7]][1])
+      #xxx[,8] <- mean(unlist(apply(xx,2,function(x) prop.table(table(x))[which(prop.table(table(x))>consensus)])))
+      xxx[,8] <- mean(mean(xx[,8]) * unlist(apply(xx,2,function(x) prop.table(table(x))[which(prop.table(table(x))>consensus)])))
+      xxx[,7] <- ifelse(!is.na(xxx[,7]) & is.na(xxx[,5]),NA,xxx[,7])
+      for(i in 7:2) xxx[,i] <- ifelse(!is.na(xxx[,i]) & is.na(xxx[,i-1]),NA,xxx[,i])
+    } else if(nrow(xx)==0) xxx[,c(1,8)] <- c("Unassigned",1)
+    return(xxx)
+  }
+
+  taxa.consensus <- lapply(X = as.list(as.data.frame(cos.table)),
+                                       FUN = machine.core)
+  taxa.consensus <- data.frame(do.call(rbind,taxa.consensus))
+  colnames(taxa.consensus) <- c("Kingdom","Phylum","Class","Order","Family","Genus","Species","Cos.score")
+  taxa.consensus <- apply(taxa.consensus, 2, as.character)
+  taxa.consensus[is.na(taxa.consensus)] <- "Unassigned"
+  taxa.consensus <- as.data.frame(taxa.consensus)
+  taxa.consensus[,8] <- as.numeric(as.character(taxa.consensus[,8]))
+  rownames(taxa.consensus) <- colnames(kmer.table)
+  return(taxa.consensus)
+}
+
+
---
+++ README.md
@@ -0,0 +1,119 @@
+# KTU
+KTU: K-mer-based taxonomic clustering algorithm improves biological relevance in microbiome associated study
+
+The 16S rDNA amplicon sequencing is widely implemented for microbiome associated studies. Microbiota feature picking algorithms for taxonomic identification and quantification are greatly renewing in recent years. The amplicon sequence variant (ASV) denoising algorithm of unbiased sequence picking replaces the OTU clustering methods. The ASV features can detect and distinguish the biological variations under the species OTU level (≧97% similarity). However, the quantification of single ASV among sequencing samples are sparse and less prevalent in the same biological groups. Here, we introduce a k-mer based, sequence alignment-free algorithm – “KTU” (K-mer Taxonomic Unit) for re-clustering ASVs into taxonomic units with more biological relevance.
+
+The “KTU” algorithm was designed with four parts (k-mer frequency counting, k-mer frequency similarity measurement, k-mer feature partitioning, and generating KTU table) and conducted in the R environment. The k-mer frequency counting was conducted by tetranucleotide frequency of amplicons; 256 tetranucleotide compositions were then converted to a 0-to-1 proportion. The similarities of k-mer frequency among amplicons were measured by cosine similarity. The similarity matrix then was converted to the distance matrix for the subsequent step. The KTUs were detected from the cosine distance matrix by using partition around medoids (PAM) clustering algorithm; the iterative PAM-KTU detecting process found the optimal cluster numbers of KTUs. The final step of the KTU algorithm was aggregating ASVs into the KTUs and generating the KTU table.
+
+We tested KTU algorithm with an open-access dataset from NCBI SRA. The SRA dataset was a wild flying squirrel fecal microbiota study that was associated with forest ambient temperature along with seasonal changes (Liu et al. 2018 from Microbial Ecology). We compared the microbiota composition associated to temperature changes using ASV dataset (r=0.52, p=0.022), KTU dataset (r=0.75, p<0.001), and the original results of paper (OTU clustering method; r=0.72, p< 0.001).
+
+The 16S amplicon denoising method unbiased picks high-quality sequence variants but generates a sparse feature table for subsequent multivariate statistical analysis. We introduce a k-mer based re-clustering method for improving the environmental and biological relevancies of ASV based microbiome associated study effectively.
+
+
+**Latest updates:** `r format(Sys.Date(), "%B %d, %Y")`
+
+### Required packages:
+`Biostrings` for parsing DNA sequence (fasta format) files
+`coop` for measuring cosine similarity of k-mer frequency
+`cluster` for conducting PAM/K-medoids clustering
+`parallel` for parallel computing
+`foreach` for parallel for-loop computing
+`doParallel` for parallel computing
+`digest` for generating MD5 hash
+
+
+## Practice
+sourcing KTU R functions
+```source("http://www.lifescipy.net/RcodeDB/FN_KTU.R") ```
+
+### Step 1 make k-mer DB
+Trim full length 16S DB to specific hypervariable regions
+Example: V3-V4 (341-805)
+Forward: **CCTACGGGNGGCWGCAG**
+Reverse: **GACTACHVGGGTATCTAATCC**
+output file: *SILVA_NR132_trimmed-sequence.fasta*
+```
+trim.primer("/home/share/SILVA_NR132/SILVA_132_QIIME_release/rep_set/rep_set_16S_only/99/silva_132_99_16S.fna",
+            forseq="CCTACGGGNGGCWGCAG",
+            revseq="GACTACHVGGGTATCTAATCC",
+            output.name="SILVA_NR132")
+```
+make and output DB
+input: *trimmed fasta file*
+output: *SILVA_132_V3V4_DB.RDS & SILVA_132_V3V4_TX.RDS*
+```
+silva.db <- makektudb(input.fasta = "SILVA_NR132_trimmed-sequence.fasta",
+                   input.taxa = "/home/share/SILVA_NR132/SILVA_132_QIIME_release/taxonomy/16S_only/99/taxonomy_7_levels.txt",
+                   output.file = "SILVA_132_V3V4")
+
+```
+
+### Step 2 run Klustering
+import ASV table (output from QIIME2 pipeline + DADA2 denoising + biom conversion) and remove original taxonomy column
+```
+data <- read.delim("/home/poyuliu/NAS2/16S_SMI/16S_sarcopenia/feature-table_w_tax.txt")
+data <- data[,-ncol(data)]
+```
+*input representive-sequence fasta file*
+Parameters:
+  iterative PAM clustering steps: 5 and 10 (default:3 and 10)
+  CPU cores: 10 (default: 1)
+```
+kluster <- klustering(repseq = "dna-sequences.fasta",
+                          feature.table = data,
+                          write.fasta = TRUE,step = c(5,10),cores = 10)
+```
+Save results
+```
+saveRDS(kluster,file="kluster.RDS")
+```
+
+### Step 3 assign k-mer taxonomy
+**similar to RDP-classifier/Naîve Bayesian algorithm**
+Read k-mer clustering result
+```
+kluster <- readRDS("kluster.RDS")
+```
+run k-mer taxonomy assignment
+input DB: {name}_DB.RDS and {name}_TX.RDS
+input k-mer frequency table from previous result: *kluster$kmer.table*
+
+```
+k.taxonomy <- kaxonomy(dbRDS = "SILVA_132_V3V4_DB.RDS",
+                      taxaRDS = "SILVA_132_V3V4_TX.RDS",
+                      kmer.table = kluster$kmer.table,
+                      cores = 3)
+```
+Save results
+```
+write.table(k.taxonomy,file = "Kaxonomy.tsv",sep="\t")
+```
+
+
+## Example
+[_Liu P.-Y., et al._ **Variations in Gut Microbiota of Siberian Flying Squirrels Correspond to Seasonal Phenological Changes in Their Hokkaido Subarctic Forest Ecosystem.** Microbial ecology 78, 223–231 (2019)](https://link.springer.com/article/10.1007/s00248-018-1278-x)
+
+
+<img src="https://media.springernature.com/full/springer-static/image/art%3A10.1007%2Fs00248-018-1278-x/MediaObjects/248_2018_1278_Fig2_HTML.png?as=webp" width="45%" /> <img src="https://media.springernature.com/full/springer-static/image/art%3A10.1007%2Fs00248-018-1278-x/MediaObjects/248_2018_1278_Fig3_HTML.png?as=webp" width="45%" />
+
+### Untreated ASV analyses
+<img src="http://www.lifescipy.net/KTU/github/github_pic_01.png" width="60%" />
+<img src="http://www.lifescipy.net/KTU/github/github_pic_02.png" width="60%" />
+<img src="http://www.lifescipy.net/KTU/github/github_pic_03.png" width="60%" />
+
+### KTU analyses
+**Phyla composition**
+<img src="http://www.lifescipy.net/KTU/github/github_pic_04.png" width="60%" />
+
+
+**Beta diversity PCoA**
+<img src="http://www.lifescipy.net/KTU/github/github_pic_05.png" width="60%" />
+
+
+**Environmental factor correlation**
+<img src="http://www.lifescipy.net/KTU/github/github_pic_06.png" width="60%" />
+<img src="http://www.lifescipy.net/KTU/github/github_pic_07.png" width="60%" />
+
+
+**Correlation heatmap**
+<img src="http://www.lifescipy.net/KTU/github/github_pic_08.png" width="60%" />
